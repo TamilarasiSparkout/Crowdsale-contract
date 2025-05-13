d3540396687181b2516f1ef17377e17fb1a64d5f
TamilarasiSparkout <tamilarasi@sparkouttech.com>
2025-05-13 12:50:35 +0530

    updated

--- contracts/newCrowdSale.sol
+++ contracts/newCrowdSale.sol
@@ -5,43 +5,43 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract newCrowdSale is Ownable {
-    uint256 public rate; // tokens per ether
+    uint256 public rate; // tokens per USDT
     IERC20 public token; // NWT tokens
+    IERC20 public USDT; // USDT tokens
 
-    event TokenPurchased(address indexed buyer, uint256 tokens);
+    event TokenPurchased(address indexed buyer,uint256 usdtAmount, uint256 tokensAmount);
     event RateChange(uint256 newRate);
-    event ETHwithdraw(uint256 amount);
-    event TokensWithdraw(uint256 totalETH);
+    event USDTwithdraw(uint256 amount);
+    event TokensWithdraw(uint256 totalUSDT);
     event UnsoldToken(uint256 nwtTokens);
 
-    constructor(uint256 _rate, address _token) Ownable(msg.sender){
+    constructor(uint256 _rate, address _token, address _usdt) Ownable(msg.sender){
         require(_rate > 0, "Rate must be > 0");
         require(_token != address(0), "Invalid token address");
         token = IERC20(_token);
+        USDT = IERC20(_usdt);
         rate = _rate;
     }
-
-    receive() external payable {
-        buyTokens();
-    }
-
+    
     function setRate(uint256 newRate) external onlyOwner {
         rate = newRate;
         emit RateChange(newRate);
     }
 
-    function buyTokens() public payable {
-        uint256 amount = msg.value * rate;
+    function buyTokens(uint256 usdtAmount) external {
+        require(usdtAmount > 0, "USDTAmount must be greater than zero");
+        uint256 amount = usdtAmount * rate;
         require(token.balanceOf(address(this)) >= amount, "Insufficient tokens in contract");
         token.transfer(msg.sender, amount);
-        emit TokenPurchased(msg.sender, amount);
+        USDT.transferFrom(msg.sender, address(this), usdtAmount);
+        emit TokenPurchased(msg.sender, usdtAmount, amount);
     }
 
-    function withdrawETH() external onlyOwner {
-        uint256 totalETH = address(this).balance;
-        require(totalETH > 0, "Insufficient ETH");
-        payable(owner()).transfer(totalETH);
-        emit ETHwithdraw(totalETH);
+    function withdrawUSDT() external onlyOwner {
+        uint256 totalUSDT = USDT.balanceOf(address(this));
+        require(totalUSDT > 0, "Insufficient USDT");
+        USDT.transfer(owner(), totalUSDT);
+        emit USDTwithdraw(totalUSDT);
     }
 
     function withdrawUnsoldToken() external onlyOwner {
